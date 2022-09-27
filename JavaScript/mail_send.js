68600004d9114a3faf201ea197ba1b73a2698bf9
Gaurav <kediagaurav1709@gmail.com>
2022-09-27 20:05:53 +0530

    Form Changes

--- JavaScript/mail_send.js
+++ JavaScript/mail_send.js
@@ -3,10 +3,10 @@ function sendEmail() {
     const email = document.getElementById("email");
     const subject = document.getElementById("subject");
     const message = document.getElementById("message");
-    console.log(name.value);
-    console.log(email.value);
-    console.log(subject.value);
-    console.log(message.value);
+    // console.log(name.value);
+    // console.log(email.value);
+    // console.log(subject.value);
+    // console.log(message.value);
     let check = "@gmail.com";
     const mes = email.value;
     if (name.value != "" && email.value != "" && subject.value != "" && mes.includes(check)) {
@@ -14,8 +14,6 @@ function sendEmail() {
             Host: "smtp.gmail.com",
             Username: name.value,
             From: email.value,
-            Password: PASS,
-            To: EMAIL,
             Subject: subject.value,
             Body: message.value,
 
